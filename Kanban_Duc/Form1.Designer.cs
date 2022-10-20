805e3d56172808c3925bd4489a521de601cccbae
Fezaaan <106581271+Fezaaan@users.noreply.github.com>
2022-10-20 14:49:51 +0200

    Button kann man bewegen

--- Kanban_Duc/Form1.Designer.cs
+++ Kanban_Duc/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hinzufügen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Task:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
--- Kanban_Duc/Form1.cs
+++ Kanban_Duc/Form1.cs
@@ -6,5 +6,132 @@ namespace Kanban_Duc
         {
             InitializeComponent();
         }
+        
+        /* Variables */
+
+        // Used to Store the Mouse Location on the Screen when Button is Clicked
+        private Point mouseDownLocation;
+
+        // Used to know if the Button can be Moved.
+        private bool isMoveable = false;
+
+        // Used to prevent clicking the button when being dragged
+        private bool clickEnabled = true;
+
+
+
+        /// <summary> Occurrs when the visible button on the Form is clicked. </summary>
+
+
+        /* Custom Button : Create & Configure the Button */
+        private void CreateButton(string btnText)
+        {
+            // Create the Button instance:
+            Button btn = new Button();
+            btn.AutoSize = true;
+            // Custom Button Style Configuration Code.
+            // i.e: FlatStyle; FlatAppearance; BackColor; ForeColor; Text; Size; Location; etc...
+            btn.FlatStyle = FlatStyle.Standard;
+            btn.FlatAppearance.BorderColor = Color.White;
+            btn.BackColor = Color.Purple;
+            btn.ForeColor = Color.White;
+            btn.Text = btnText;
+            btn.Location = new Point(100, 100);
+
+            // Custom Button Event.
+            btn.Click += new EventHandler(CustomButton_Click);
+            btn.MouseDown += new MouseEventHandler(CustomButton_MouseDown);
+            btn.MouseMove += new MouseEventHandler(CustomButton_MouseMove);
+            btn.MouseUp += new MouseEventHandler(CustomButton_MouseUp);
+
+            // Add Button to Control Collection.
+            Controls.Add(btn);
+
+            // Show Button.
+            btn.Show();
+        }
+
+
+        /* Custom Button Events */
+
+        /// <summary> Occurrs whenever the Custom Button is Clicked </summary>
+        private void CustomButton_Click(object sender, EventArgs e)
+        {
+            if (clickEnabled)
+            {
+                // Note: The "for" loop bellow lets you get the current button; so you can drag it.
+
+                Button btn = (Button)sender;
+
+                // Iterate over the Form Controls
+                for (int i = 0; i < Controls.Count; i++)
+                {
+                    // If the Button Clicked Corresponds to a Found Index in Control Collection...
+                    if (i == Controls.IndexOf(btn))
+                    {
+                        // Perform Corresponding Button Action.
+                        MessageBox.Show(btn.Name + " Clicked!");
+                    }
+                }
+            }
+        }
+
+        /// <summary> Occurrs whenever Left Mouse Button Clicks the Button and is Kept Down. </summary>
+        private void CustomButton_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Check if Left Mouse Button is Clicked.
+            if (e.Button == MouseButtons.Left)
+            {
+                // Set mouseDownLocation (Point) variable, according to the current mouse location (X and Y).
+                mouseDownLocation = e.Location;
+
+                // Enable the hability to move the Button.
+                isMoveable = true;
+            }
+        }
+
+        /// <summary> Occurrs whenever Left Mouse Button is Down and Mouse is Moving the Button. </summary>
+        private void CustomButton_MouseMove(object sender, MouseEventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            // If the hability to move the button is "true" and the mouse button used to drag the button is the left mouse button:
+            if (isMoveable)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    // Set the Button location X and Y...
+                    btn.Left = e.X + btn.Left - mouseDownLocation.X;
+                    btn.Top = e.Y + btn.Top - mouseDownLocation.Y;
+                }
+            }
+
+            // Disable Click hability (See: Note at the end of this post).
+            clickEnabled = false;
+        }
+
+        /// <summary> Occurrs whenever Left Mouse Button is not longer being hold down (Left Mouse Button is Up). </summary>
+        private void CustomButton_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Disables the hability to move the button
+            isMoveable = false;
+            clickEnabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Size size = new Size(200,30);
+            String box_text = textBox1.Text;
+            // Create the Button. Specifying the Button Text and its Size.
+            CreateButton(box_text);
+            textBox1.Text = "";
+        }
+
+        
     }
 }
